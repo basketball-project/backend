f15b0b477df451f4c083d6c80b72d8cd4325ac63
KONG JI HOON <cc127923@gmail.com>
2024-07-23 16:55:45 +0900

    first commit

---
+++ src/main/java/com/example/basketballmatching/BasketballMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.basketballmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballMatchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketballMatchingApplication.class, args);
+    }
+
+}
